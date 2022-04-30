f6af6ec36519d50ba0a0b51c1106d85b21d876af
QuimTorra <q.torra@gmail.com>
2022-04-30 04:26:30 +0200

    Al menys funciona un putu mapa (que ni eso pero bueno xd)

---
+++ LiuLan2/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import HMSMap, { HMSMarker, MapTypes } from '@hmscore/react-native-hms-map';
+
+const App = () => {
+  // AGCApp.getInstance().setClientId('clientId');
+  // AGCApp.getInstance().setClientSecret('clientSecret');
+  // AGCApp.getInstance().setApiKey('apiKey');
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  return (
+    <>
+      <HMSMap 
+        mapType={MapTypes.NORMAL}
+        style={{ height: 400 }}
+        camera={{ target: { latitude: 41, longitude: 29 }, zoom: 11 }}
+      >
+        <HMSMarker // Simple example
+          coordinate={{ latitude: 41, longitude: 29 }}
+          title="Hello Huawei Map"
+          snippet="This is a snippet!"
+        />
+      </HMSMap>
+      
+    </>
+  );
+};
+
+export default App;
